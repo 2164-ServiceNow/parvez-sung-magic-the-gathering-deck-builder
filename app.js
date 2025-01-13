e05e47b14ef09f2a7ae92a6adab97fbcc447dc1b
Timothy Sung <sungtimo@msu.edu>
2025-01-13 16:32:58 -0500

    Added searchBar and cards controller

--- app.js
+++ app.js
@@ -1,22 +1,18 @@
-let app = angular.module("magicApp", ["ngRoute"]);
-app.config(function($routeProvider, $locationProvider) {
+'use strict';
+
+angular.module("magicApp", ['ngRoute', 'cards', 'searchBar'])
+.config(function($routeProvider, $locationProvider) {
     $routeProvider
         .when("/", {
             templateUrl: "app/pages/main.html"
         })
         .when("/cards", {
-            templateUrl: "app/pages/cards.html"
+            templateUrl: "app/pages/cardsPage/cards.html",
+            controller: "cardsCtrl"
         });
 
     $locationProvider.html5Mode({
         enabled: true,
         requireBase: false
     });
-});
-
-app.directive("navbar", () => {
-    return {
-        restrict: 'E',
-        templateUrl: "app/pages/navbar.html"
-    };
 });
---
+++ app/pages/cardsPage/cards.js
@@ -0,0 +1,5 @@
+angular.module('cards', [])
+
+.controller('cardsCtrl', function($scope){
+    $scope.cardName = "Archangel Avacyn"
+})
