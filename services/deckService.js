df3f0671b0b456fbe745c6403eacf9e9d3c02fa1
TimmyKSung <timmyksung@gmail.com>
2025-01-22 22:56:59 -0500

    Implemented deck statistics functionality and deck page styling

--- services/deckService.js
+++ services/deckService.js
@@ -55,8 +55,8 @@ angular
       let cards = this.decks[deckIndex].cards
       const colorSet = new Set()
       cards.forEach(card => {
-        if (Array.isArray(card.colors)) {
-          card.colors.forEach(color => colorSet.add(color))
+        if (Array.isArray(card.colorIdentity)) {
+          card.colorIdentity.forEach(color => colorSet.add(color))
         }
       })
 
@@ -65,5 +65,46 @@ angular
 
     this.broadcastDecksChange = function() {
       $rootScope.$broadcast("decksChange");
+    }
+
+    this.getCMCGraph = function(deckIndex) {
+      let cards = this.decks[deckIndex].cards
+      let cmcCount = {}
+      cards.forEach(card => {
+        if (typeof card.cmc === 'number') {
+          cmcCount[card.cmc] = (cmcCount[card.cmc] || 0) + 1;
+        }
+      })
+      return cmcCount
+    }
+
+    this.getRarityGraph = function(deckIndex) {
+      let cards = this.decks[deckIndex].cards
+      let rarityCount = {}
+      cards.forEach(card => {
+        if (typeof card.rarity === 'string') {
+          rarityCount[card.rarity] = (rarityCount[card.rarity] || 0) + 1;
+        }
+      })
+      return rarityCount
+    }
+
+    this.getNumberOfLands = function(deckIndex) {
+      let cards = this.decks[deckIndex].cards
+      let landCount = 0
+      cards.forEach(card => {
+        if (Array.isArray(card.types) && card.types[0] === 'Land') {
+          landCount += 1
+        }
+      })
+      return landCount
+    }
+
+    this.getDeckStatistics = function(deckIndex) {
+      this.decks = this.getDecks()
+      let cmcGraph = this.getCMCGraph(deckIndex)
+      let rarityGraph = this.getRarityGraph(deckIndex)
+      let lands = this.getNumberOfLands(deckIndex)
+      return [cmcGraph, rarityGraph, lands, this.decks[deckIndex].cards.length]
     }
   })
