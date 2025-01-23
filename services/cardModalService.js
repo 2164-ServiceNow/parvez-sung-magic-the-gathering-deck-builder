7d2dcce96f4f43545583dd377b8c81aa55faa68a
aparvez1 <aparvez85@outlook.com>
2025-01-23 00:22:57 -0500

    created modals for addToDeck, added functionality for favorites add and deleted

--- services/cardModalService.js
+++ services/cardModalService.js
@@ -2,9 +2,12 @@ angular
     .module("magicApp")
     .service("cardModalService", function () {
         this.card = []
+        this.imgPlaceHolder=''
 
         this.setCard = function(card) {
-          if (card) { this.card = card }
+          if (card) { this.card = card 
+            // this.imgPlaceHolder = imgPlaceHolder;
+          }
         }
 
         this.getCard = function() {
